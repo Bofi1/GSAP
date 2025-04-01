7fe2c2f60e100b2fe5b1edc713404b59201bf41b
Bofi1 <angelborque03github@gmail.com>
2025-04-01 14:10:41 -0700

    onUpdate

--- scrollTrigger/ej. 4 (onEnter, leave, enterback y leaveback (functions))/app.js
+++ scrollTrigger/ej. 4 (onEnter, leave, enterback y leaveback (functions))/app.js
@@ -1,15 +1,20 @@
 gsap.registerPlugin(ScrollTrigger)
 
-// se puede usar funciones cuando pasen los 
-// onEnter     onLive      onEnterBack     onLeaveBack   
+ 
 ScrollTrigger.create({
     trigger: ".box",
     start: "top 80%",
     end: "bottom 20%",
     markers: true,
-    onEnter: () => {console.log("Enter");},
-    onLeave: () => {console.log("Leave");},
-    onEnterBack: () => {console.log("enterBack");},
-    onLeaveBack: () => {console.log("leaveBack");} 
+
+    // se puede usar funciones cuando pasen los 
+        // onEnter     onLive      onEnterBack     onLeaveBack  
+    
+    onUpdate: (self) => {console.log(self)} // cuando se hace eso se va mostrando el progreso del scroll referente al trigger
+    
+    // onEnter: () => {console.log("Enter");},
+    // onLeave: () => {console.log("Leave");},
+    // onEnterBack: () => {console.log("enterBack");},
+    // onLeaveBack: () => {console.log("leaveBack");} 
 })
 
